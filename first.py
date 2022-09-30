a0544fe1227fe9b3b41c365f9e525459ef876522
arunkudiyal <arunkudiyal@icloud.com>
2022-09-30 09:51:33 +0530

    Variables, Data Types, Strings

---
+++ first.py
@@ -0,0 +1,59 @@
+# Data
+
+# Operators --> +, -, *, /
+# print( 10 + 50 )
+# print( 100 - 50 )
+# print( 10 * 5 )
+# print( 10 / 2 ) # Quotient
+
+# # Modulous
+# print( 10 % 2 ) # Remiander
+# # power
+# print(7 ** 2)
+
+# print( (10 + 20) * 2 )
+# print( 10 + 20 * 2 ) # Python used the concept of BODMAS for expression evaluation
+
+
+# VARIBALES
+
+# variableOne = 10 + 20
+# print( variableOne * 2 )
+
+
+# Diff Types of data
+
+# r_no = 12217871
+# name = "Shashvat Mishra"
+# isGraduated = False
+# percentage = 92.78
+
+# print( type(r_no) ) # int
+# print( type(name) ) # string
+# print( type(isGraduated) )  # boolean
+# print( type(percentage) )   # float
+
+# print( 10 / 2 ) # using '/' --> Result is always in float
+# print( 10 // 2 ) # using '//' --> Result is always in int
+
+
+# STRING
+
+''' This is a multi line comment '''
+
+stringOne = "This is String One"
+stringTwo = 'This is String Two'
+# print(stringOne)
+# print(stringTwo)
+
+# Operations
+# print( stringOne + " " + stringTwo )  # String Concatenation
+# print( stringOne - stringTwo )  # String Concatenation
+# print( stringOne * stringTwo )  # String Concatenation
+# print( stringOne / stringTwo )  # String Concatenation
+
+stringThree = "100"
+stringFour = "10"
+# print( stringThree - stringFour )  # String Concatenation
+# print( stringThree * stringFour )  # String Concatenation
+# print( stringThree / stringFour )  # String Concatenation
